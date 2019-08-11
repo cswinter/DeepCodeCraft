4af9395a6535b34ef9ae47ed5e7cacfe65a5bcd8
Clemens Winter <clemenswinter1@gmail.com>
2019-08-11 09:59:31 -0700

    Fix boolean args

--- hyper_params.py
+++ hyper_params.py
@@ -9,14 +9,14 @@ class HyperParams:
         self.lr = 0.1               # Learning rate
         self.momentum = 0.9         # Momentum
         self.bs = 128               # Batch size during optimization
-        self.shuffle = True
+        self.shuffle = False        # Shuffle samples collected during rollout before optimization
 
         # Policy
         self.depth = 3              # Number of hidden layers
         self.width = 1024           # Number of activations on each hidden layer
 
         # RL
-        self.steps = 4e6            # Total number of timesteps
+        self.steps = 2e6            # Total number of timesteps
         self.seq_rosteps = 64       # Number of sequential steps per rollout
         self.rosteps = 64 * 32      # Number of total rollout steps
         self.gamma = 0.8            # Discount factor
@@ -29,6 +29,9 @@ class HyperParams:
     def args_parser(self) -> argparse.ArgumentParser:
         parser = argparse.ArgumentParser()
         for name, value in vars(self).items():
+            if isinstance(value, bool):
+                parser.add_argument(f"--no-{name}", action='store_false', dest=name)
+                parser.add_argument(f"--{name}", action='store_true', dest=name)
             parser.add_argument(f"--{name}", type=type(value))
         return parser
 
