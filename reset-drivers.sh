889806957b70272e8890b7b391591b925c42f9f7
Clemens Winter <clemenswinter1@gmail.com>
2020-01-09 06:27:51 -0800

    Add script to reset GPU drivers

---
+++ reset-drivers.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo rmmod nvidia_uvmsudo rmmod nvidia_drm
+sudo rmmod nvidia_modeset
+sudo rmmod nvidia
+sudo modprobe nvidia
+sudo modprobe nvidia_modeset
+sudo modprobe nvidia_drm
+sudo modprobe nvidia_uvm
