1fb67eef4d6359a35831b6c485179dfc51de82ba
Clemens Winter <clemenswinter1@gmail.com>
2020-08-16 13:29:19 -0700

    Logging, tweak initialization, add gate

--- gaussian_attention.py
+++ gaussian_attention.py
@@ -5,10 +5,11 @@ from torch.nn.init import uniform_
 
 
 class GaussianAttention(nn.Module):
-    def __init__(self, nhead: int, scale: float):
+    def __init__(self, nhead: int, scale: float, optional: bool = True):
         super(GaussianAttention, self).__init__()
         self.nhead = nhead
         self.scale = scale
+        self.optional = optional
 
         self.mean = Parameter(torch.Tensor(nhead))
         self.logvariance = Parameter(torch.Tensor(nhead))
@@ -23,11 +24,19 @@ class GaussianAttention(nn.Module):
         variance = torch.exp(self.logvariance).view(1, self.nhead, 1, 1)
         centered = (rescaled.view(dbatch, 1, dseq1, dseq2) - self.mean.view(1, self.nhead, 1, 1)) / variance
         density = torch.exp(-centered.pow(2))
-        return density
+        if self.optional:
+            weight = torch.sigmoid(self.weight).view(1, self.nhead, 1, 1)
+        else:
+            weight = 1.0
+        return 1 + weight * (density - 1)
 
     def _reset_parameters(self):
-        uniform_(self.mean, -0.1, 0.1)
-        uniform_(self.logvariance, -1, 1)
+        uniform_(self.mean, 0.0, 0.1)
+        uniform_(self.logvariance, -0.5, 0.5)
+        uniform_(self.weight, -0.5, 0.5)
+        print("Initial mean: ", self.mean)
+        print("Initial logvariance: ", self.logvariance)
+        print("Initial weight: ", self.weight)
 
 
 if __name__ == '__main__':
