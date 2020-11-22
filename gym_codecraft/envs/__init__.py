c8b587d3313b5ee0577605e86b6eb2bcf08605fe
Clemens Winter <clemenswinter1@gmail.com>
2020-11-22 14:02:12 -0800

    Remove dependency on openai/gym

--- gym_codecraft/envs/__init__.py
+++ gym_codecraft/envs/__init__.py
@@ -1,3 +1,2 @@
-from gym_codecraft.envs.codecraft_env import CodeCraftEnv
 from gym_codecraft.envs.codecraft_vec_env import CodeCraftVecEnv
 from gym_codecraft.envs.codecraft_vec_env import Objective
