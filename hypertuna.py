41f1e28c0929365be58c4efaf6322723c759bc22
Clemens Winter <clemenswinter1@gmail.com>
2021-10-25 18:47:31 -0700

    Tweaks

--- hypertuna.py
+++ hypertuna.py
@@ -12,7 +12,7 @@ import threading
 
 from enum import Enum
 
-from hyperstate import Config, _load_file_and_schedules
+from hyperstate import Config, HyperState, _load_file_and_schedules
 
 
 class SamplingStrategy(Enum):
@@ -93,6 +93,9 @@ hyper_params = {
         sampling_strategy=SamplingStrategy.POWER_OF_TWO,
         min_value=128,
     ),
+    "vf_coef": HyperParam(
+        path="optimizer.vf_coef", sampling_strategy=SamplingStrategy.LOGUNIFORM,
+    )
 }
 
 
@@ -117,6 +120,7 @@ class HyperOptimizer:
         self.cvar = threading.Condition(self.lock)
         self.running_xps = 0
         self.parallelism = parallelism
+        self.base_config_path = base_config_path
 
         hpconfig, schedules = _load_file_and_schedules(Config, base_config_path, [])
         params_with_center = []
@@ -135,7 +139,7 @@ class HyperOptimizer:
         xp = deepcopy(self.config)
         xp.containers[0].command += [
             "--config",
-            "configs/arena_medium.yaml",
+            self.base_config_path,
             "--hps",
             f"ppo.steps={self.steps}",
         ]
@@ -220,149 +224,5 @@ if __name__ == "__main__":
         path, r = param.split("=")
         params.append((path, float(r),))
     HyperOptimizer(args.base_config_path, params, args.parallelism).run(args.n_trials)
-
-
-class LegacyHyperOptimizer:
-    def __init__(self):
-        self.xprun = xprun.Client()
-        self.wandb = wandb.Api()
-        self.trial = 0
-        self.xp_name = f"optuna-{random.randint(0, 0xffffff):06x}"
-        xp = xprun.build_xpdef(
-            "xprun.ron", ignore_dirty=False, include_dirty=False, verbose=False,
-        )
-        xp.base_name = self.xp_name
-        self.config = xp
-
-    def objective(self, trial: optuna.trial.Trial):
-        # return self.allied_wealth(trial)
-        return self.arena_medium(trial)
-
-    def arena_medium(self, trial: optuna.trial.Trial):
-        steps = int(5e6)
-        lr = trial.suggest_loguniform("lr", 1e-5, 1e-2)
-        final_lr_mult = trial.suggest_loguniform("final_lr_mult", 1e-2, 1)
-        lg_seq_rosteps = trial.suggest_int("lg_seq_rosteps", 3, 8)
-        seq_rosteps = 2 ** lg_seq_rosteps
-        lg_num_envs = trial.suggest_int("lg_num_envs", 7, 10)
-        num_envs = 2 ** lg_num_envs
-        batch_size = 2 ** trial.suggest_int(
-            "lg_batch_size", 9, min(14, lg_seq_rosteps + lg_num_envs)
-        )
-        ommomentum = trial.suggest_loguniform("ommomentum", 0.01, 1)
-        momentum = 1 - ommomentum
-        weight_decay = trial.suggest_loguniform("weight_decay", 1e-6, 1e-2)
-        epochs = trial.suggest_int("epochs", 1, 3)
-        omgamma = trial.suggest_loguniform("omgamma", 0.0001, 1)
-        gamma = 1 - omgamma
-        cliprange = trial.suggest_uniform("cliprange", 0.05, 0.5)
-        entropy_bonus1 = trial.suggest_loguniform("entropy_bonus1", 0.1, 10)
-        entropy_bonus2 = trial.suggest_loguniform("entropy_bonus2", 0.01, 1)
-        micro_batch_size = min(2048, batch_size)
-        xp = deepcopy(self.config)
-        xp.containers[0].command += [
-            "--config",
-            "configs/arena_medium.yaml",
-            "--hps",
-            f"ppo.steps={steps}",
-            f"optimizer.lr=step: {lr}@0 cos {lr*final_lr_mult}@{steps}",
-            f"optimizer.batch_size={batch_size}",
-            f"optimizer.micro_batch_size={micro_batch_size}",
-            f"optimizer.momentum={momentum}",
-            f"optimizer.weight_decay={weight_decay}",
-            f"optimizer.epochs={epochs}",
-            f"optimizer.entropy_bonus=step: {entropy_bonus1}@0 lin {entropy_bonus2}@{steps // 2} 0.0@{steps}",
-            f"ppo.cliprange={cliprange}",
-            f"ppo.gamma={gamma}",
-            f"ppo.seq_rosteps={seq_rosteps}",
-            f"ppo.num_envs={num_envs}",
-            f"ppo.num_self_play={num_envs // 2}",
-            f"--descriptor=lr={lr:.2e},final_lr={lr*final_lr_mult:.2e},batch_size={batch_size},seq_rosteps={seq_rosteps},num_envs={num_envs},momentum={momentum:.2e},weight_decay={weight_decay:.2e},epochs={epochs},cliprange={cliprange:.2e},gamma={gamma:.2e}",
-        ]
-        xp.name = f"{self.xp_name}-{self.trial}"
-
-        self.trial += 1
-        self.run_xp(xp)
-        run = list(
-            self.wandb.runs("cswinter/deep-codecraft-vs", {"config.xp_name": xp.name})
-        )[0]
-        return run.summary.get("eval_mean_score", -1)
-
-    def allied_wealth(self, trial: optuna.trial.Trial):
-        lr = trial.suggest_loguniform("lr", 1e-6, 1)
-        final_lr_mult = trial.suggest_loguniform("final_lr_mult", 1e-2, 1)
-        lg_seq_rosteps = trial.suggest_int("lg_seq_rosteps", 3, 8)
-        seq_rosteps = 2 ** lg_seq_rosteps
-        lg_num_envs = trial.suggest_int("lg_num_envs", 6, 10)
-        num_envs = 2 ** lg_num_envs
-        batch_size = 2 ** trial.suggest_int(
-            "lg_batch_size", 8, min(14, lg_seq_rosteps + lg_num_envs)
-        )
-        ommomentum = trial.suggest_loguniform("ommomentum", 0.01, 1)
-        momentum = 1 - ommomentum
-        weight_decay = trial.suggest_loguniform("weight_decay", 1e-6, 1e-2)
-        epochs = trial.suggest_int("epochs", 1, 3)
-        omgamma = trial.suggest_loguniform("omgamma", 0.001, 1)
-        gamma = 1 - omgamma
-        omlambda = trial.suggest_loguniform("omlambda", 0.01, 1)
-        lamb = 1 - omlambda
-        cliprange = trial.suggest_uniform("cliprange", 0.05, 0.5)
-        micro_batch_size = min(2048, batch_size)
-        xp = deepcopy(self.config)
-        xp.containers[0].requests[0].resources["memory"] = int(3.5e9)
-        xp.containers[0].command += [
-            "--config",
-            "configs/allied_wealth.yaml",
-            "--hps",
-            "ppo.steps=250e3",
-            f"optimizer.lr=step: {lr}@0 cos {lr*final_lr_mult}@250e3",
-            f"optimizer.batch_size={batch_size}",
-            f"optimizer.micro_batch_size={micro_batch_size}",
-            f"optimizer.momentum={momentum}",
-            f"optimizer.weight_decay={weight_decay}",
-            f"optimizer.epochs={epochs}",
-            f"ppo.cliprange={cliprange}",
-            f"ppo.gamma={gamma}",
-            f"ppo.lamb={lamb}",
-            f"ppo.seq_rosteps={seq_rosteps}",
-            f"ppo.num_envs={num_envs}",
-            f"--descriptor=lr={lr:.2e},final_lr={lr*final_lr_mult:.2e},batch_size={batch_size},seq_rosteps={seq_rosteps},num_envs={num_envs},momentum={momentum:.2e},weight_decay={weight_decay:.2e},epochs={epochs},cliprange={cliprange:.2e},gamma={gamma:.2e},lamb={lamb:.2e}",
-        ]
-        xp.name = f"{self.xp_name}-{self.trial}"
-        self.trial += 1
-        self.run_xp(xp)
-        run = list(
-            self.wandb.runs("cswinter/deep-codecraft", {"config.xp_name": xp.name})
-        )[0]
-        return run.summary.get("eprewmean", 0)
-
-    def run_xp(self, xp):
-        for retry in range(10):
-            try:
-                self.xprun.run(xp, wait=True, priority=3, user="clemens")
-            except Exception as e:
-                print(f"Failed to run {xp.name}: {e}")
-                print(f"Retrying in 60 seconds... ({retry})")
-                time.sleep(60)
-                continue
-            break
-        else:
-            print(f"Failed to run {xp.name}")
-            return 0
-        while True:
-            try:
-                self.xprun.block_until_completed(xp.name)
-                break
-            except Exception as e:
-                print(f"Failed to block_until_completed {xp.name}: {e}")
-                print(f"Retrying in 60 seconds... ({retry})")
-                time.sleep(60)
-                continue
-
-    def optimize(self):
-        study = optuna.create_study(direction=optuna.study.StudyDirection.MAXIMIZE)
-        study.optimize(self.objective, n_trials=500, n_jobs=6)
-        print(study.best_params)
-
 
 # python hypertuna.py --base_config_path=configs/arena_medium_10m.yaml --params lr=10 gamma=10 batch_size=4 weight_decay=10 entropy_bonus=10 cliprange=0.1 seq_rosteps=4 num_envs=4
